60dc1f955c1e3073d631e6a995d0c362aa0c6880
jack-hug <46361381@qq.com>
2019-12-06 01:19:44 +0800

    数据库结构

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'wen hai yan and unice'
 bootstrap = Bootstrap(app)
 
 @app.route('/')
@@ -76,7 +77,7 @@ def ability_team():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
 
 
 
