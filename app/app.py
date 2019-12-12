5ca2ed2a3f8b55363714fd0ce16b07534d019d81
jack-hug <46361381@qq.com>
2019-12-13 00:47:12 +0800

    index design

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('base.html')
 
 @app.route('/about_us')
 def about_us():
