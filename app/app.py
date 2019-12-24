b5eee935d2e6677ad241f27e89249ba8a9530433
jack-hug <46361381@qq.com>
2019-12-25 00:25:14 +0800

    新版本 产品图片

--- app/app.py
+++ app/app.py
@@ -14,70 +14,77 @@ def index():
 def mold_making():
     return render_template('services/mold_making.html')
 
+@app.route('/advantages')
+def advantages():
+    return render_template('services/advantages.html')
+
+@app.route('/design_engineering')
+def design_engineering():
+    return render_template('services/design_engineering.html')
+
+@app.route('/injection_molding')
+def injection_molding():
+    return render_template('services/injection_molding.html')
+
+@app.route('/Project_management')
+def Project_management():
+    return render_template('services/Project_management.html')
+
 @app.route('/about_us')
 def about_us():
-    return render_template('about_us.html')
+    return render_template('about_us/about_us.html')
 
 @app.route('/contact_us')
 def contact_us():
-    return render_template('contact_us.html')
+    return render_template('contact_us/contact_us.html')
 
-@app.route('/recruitment')
-def recruitment():
-    return render_template('recruitment.html')
+@app.route('/Insert_Molding')
+def Insert_Molding():
+    return render_template('products/Insert_Molding.html')
 
-@app.route('/products_Insert_Molding')
-def products_Insert_Molding():
-    return render_template('products_Insert_Molding.html')
+@app.route('/Electrical_Plastics_Molding')
+def Electrical_Plastics_Molding():
+    return render_template('products/Electrical_Plastics_Molding.html')
 
-@app.route('/products_Electrical_Plastics_Molding')
-def products_Electrical_Plastics_Molding():
-    return render_template('products_Electrical_Plastics_Molding.html')
+@app.route('/Rubber')
+def Rubber():
+    return render_template('products/Rubber.html')
 
-@app.route('/products_Rubber')
-def products_Rubber():
-    return render_template('products_Rubber.html')
+@app.route('/Auto_Plastics_Part')
+def Auto_Plastics_Part():
+    return render_template('products/Auto_Plastics_Part.html')
 
-@app.route('/products_Auto_Plastics_Part')
-def products_Auto_Plastics_Part():
-    return render_template('products_Auto_Plastics_Part.html')
+@app.route('/2k_Molding')
+def twok_Molding():
+    return render_template('products/2k_Molding.html')
 
-@app.route('/products_2k_Molding')
-def products_2k_Molding():
-    return render_template('products_2k_Molding.html')
-
-@app.route('/products_Casting')
-def products_Casting():
-    return render_template('products_Casting.html')
+@app.route('/Casting')
+def Casting():
+    return render_template('products/Casting.html')
 
 @app.route('/Stamping tool_1')
 def Stamping_tool_1():
-    return render_template('Stamping_tool_1.html')
+    return render_template('products/Stamping_tool_1.html')
 
 @app.route('/Stamping_tool_2')
 def Stamping_tool_2():
-    return render_template('Stamping_tool_2.html')
+    return render_template('products/Stamping_tool_2.html')
 
 @app.route('/Stamping_tool_3')
 def Stamping_tool_3():
-    return render_template('Stamping_tool_3.html')
+    return render_template('products/Stamping_tool_3.html')
 
 @app.route('/plastic')
 def plastic():
-    return render_template('plastic.html')
+    return render_template('products/plastic.html')
 
-@app.route('/ability_equipment')
-def ability_equipment():
-    return render_template('ability_equipment.html')
+@app.route('/quality_assurance')
+def quality_assurance():
+    return render_template('quality/quality_assurance.html')
 
-@app.route('/ability_quality')
-def ability_quality():
-    return render_template('ability_quality.html')
-
-@app.route('/ability_team')
-def ability_team():
-    return render_template('ability_team.html')
-
+@app.route('/process_flowchart')
+def process_flowchart():
+    return render_template('quality/process_flowchart.html')
 
 
 if __name__ == "__main__":
