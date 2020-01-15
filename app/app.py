a05e0343c8f9687e9c80dbec3171f3694265a717
jack-hug <46361381@qq.com>
2020-01-15 16:42:53 +0800

    数据库

--- app/app.py
+++ app/app.py
@@ -1,10 +1,69 @@
 from flask import Flask,render_template,redirect,url_for
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from datetime import datetime
+import os,mysql.connector
 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'wen hai yan and unice'
 bootstrap = Bootstrap(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:HUANGzeng123@localhost/unice-dev'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
+manager = Manager(app)
+
+# 定义模型
+class Information(db.Model):
+    __tablename__ = 'information'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64), unique=True)
+    banner = db.Column(db.Text())
+    inf_img = db.Column(db.Text())
+    content = db.Column(db.String(64))
+    add_time = db.Column(db.DateTime(),default = datetime.utcnow)
+
+    def __repr__(self):
+        return '<Role %r>' % self.title
+
+class Product_class(db.Model):
+    __tablename__ = 'product_class'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    is_enable = db.Column(db.Integer)
+    add_time = db.Column(db.DateTime(),default = datetime.utcnow)
+    product = db.relationship('Product',backref = 'product_class')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class Product(db.Model):
+    __tablename__ = 'product'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    code = db.Column(db.Text())
+    product_class_id = db.Column(db.Integer,db.ForeignKey('product_class.id'))
+    product_img = db.Column(db.Text())
+    is_enable = db.Column(db.Integer)
+    add_time = db.Column(db.DateTime(),default = datetime.utcnow)
+
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<Role %r>' % self.username
 
 @app.route('/')
 def index():
@@ -88,7 +147,7 @@ def process_flowchart():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    manager.run()
 
 
 
