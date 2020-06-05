41199e6615d58469e11643638f24a202b47d3b07
jack-hug <46361381@qq.com>
2020-06-05 16:58:55 +0800

    bs4 mobile 0605

--- app/static/js/unice.js
+++ app/static/js/unice.js
@@ -1,25 +1,8 @@
-//监听滚动事件
-window.onscroll = function() {
-    // 滚动时当前位置距顶部的距离
-    var topScroll = document.documentElement.scrollTop;
-    console.log(topScroll)
-        // 获取导航id
-    var nav = document.getElementById("mynavbar");
-    // 滚动距离大于多少时执行下面事件
-    if (topScroll > 150) {
-        // 到了那个距离相当于给了导航定位
-        nav.style.position = 'fixed';
-        nav.style.top = '0';
-        nav.style.zIndex = '99';
-        nav.style.width = '100%';
-    } else {
-        // 小于的时候让他恢复原状
-        nav.style = ''
-    }
-}
-
-// 点击时自动切换active
-$(document).ready(function() {
-    $('li.active').removeClass('active');
-    $('a[href="' + location.pathname + '"]').closest('li').addClass('active'); 
-  });
+// carousel follow navbar bottom
+var onResize = function() {
+    $('body').css('padding-top',$('.fixed-top').height()+28);
+};
+$(window).resize(onResize);
+$(function() {
+    onResize();
+});
